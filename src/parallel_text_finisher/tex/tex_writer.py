9796a7a918914544fa73dfe528c0cdeefdb2ce56
yelircaasi <isaac.r.riley@gmail.com>
2023-04-03 08:18:24 +0200

    push before timeshift

--- src/parallel_text_finisher/tex/tex_writer.py
+++ src/parallel_text_finisher/tex/tex_writer.py
@@ -1,5 +1,17 @@
+from pathlib import Path
+from typing import Optional, Tuple
+
+from dynaconf import Dynaconf
+
+
+class TexWriter:
+    def __init__(self, input_file_paths: Tuple[Path, Path]) -> None:
+        self.input_file_paths = input_file_paths
+
+
 class TexWriterBuilder:
-    ...
+    def __init__(self, cfg: Dynaconf) -> None:
+        self.cfg = cfg
 
 
 def get_tex_writer(settings_object):
