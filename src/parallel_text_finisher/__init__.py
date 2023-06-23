722327d20bcd6a10595d4a4be79e12004b2088f9
yelircaasi <isaac.r.riley@gmail.com>
2023-06-23 05:48:34 +0200

    add additional structure

--- src/parallel_text_finisher/__init__.py
+++ src/parallel_text_finisher/__init__.py
@@ -1,4 +1,4 @@
-from parallel_text_finisher.config import settings
+from parallel_text_finisher.config.config import settings
 from parallel_text_finisher.finisher import get_finisher
 
 __all__ = ["get_finisher", "settings"]
